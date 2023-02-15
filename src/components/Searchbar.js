e0552168b373995c1f8242c364ff7d9981eac8fa
Grahambone37 <electricgrunt37@gmail.com>
2023-02-14 23:30:59 -0800

    searchbar sends data to a function in app

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,8 +5,11 @@ export default function Searchbar(props) {
 
     return (
         <div>
-            <form>
-                <input type="text" placeholder="Enter a search term here"></input>
+            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+                <input type="text"
+                    placeholder="Enter a search term here"
+                    onChange={(e) => setSearchTerm(e.target.value)}>
+                </input>
                 <input type="submit"></input>
             </form>
         </div>
